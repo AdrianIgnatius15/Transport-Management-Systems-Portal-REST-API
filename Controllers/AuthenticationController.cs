11a6999bca480d363f1a96d7ca59ad8d4928af14
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-09 21:39:04 +0800

    Created initial endpoints

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,26 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using Transport_Management_Systems_Portal_REST_API.Data.Interfaces;
 using Transport_Management_Systems_Portal_REST_API.DTOs;
+using Transport_Management_Systems_Portal_REST_API.DTOs.Request_Models;
+using Transport_Management_Systems_Portal_REST_API.Models;
+using Transport_Management_Systems_Portal_REST_API.Utilities;
 
 namespace Transport_Management_Systems_Portal_REST_API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        public AuthenticationController()
-        {
+        private readonly IAuthenticationRepo _authenticationRepo;
 
+        public AuthenticationController(IAuthenticationRepo authenticationRepo)
+        {
+            _authenticationRepo = authenticationRepo;
         }
 
-        public IActionResult Login()
+        [HttpPost("login")]
+        public async Task<ActionResult<UserReadDto>> Login(LoginRequest loginRequest)
         {
-            var userDto = new UserReadDto
+            var searchedUser = await _authenticationRepo.GetUserAsync(loginRequest.Email, loginRequest.Password);
+
+            if (searchedUser == null)
             {
-                Id = 1,
-                Username = "testuser",
-                Email = "testuser@example.com",
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
-            };
-            return Ok(userDto);
+                return BadRequest($"--> Login failed due to invalid credentials or account do not exists!");
+            }
+            else
+            {
+                var userReadDto = MapperUtility.Map<User, UserReadDto>(searchedUser);
+
+                return Ok(userReadDto);
+            }
+
         }
     }
 }
