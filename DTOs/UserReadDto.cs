bbe4fe083c5b4632dc1e35ae18f3294a762c8927
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-09 18:46:04 +0800

    Created a `UserReadDto` for `User`

---
+++ DTOs/UserReadDto.cs
@@ -0,0 +1,8 @@
+public record UserReadDto
+{
+    public int Id { get; init; }
+    public string Username { get; init; }
+    public string Email { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; set; }
+}
