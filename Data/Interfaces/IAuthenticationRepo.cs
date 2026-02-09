d9bfc9a3fa5f55d434cc5d995f3ce81d83b41cd4
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-09 21:38:32 +0800

    Created a "contract first" dependency injectable repository for `User`

---
+++ Data/Interfaces/IAuthenticationRepo.cs
@@ -0,0 +1,13 @@
+using Transport_Management_Systems_Portal_REST_API.Models;
+
+namespace Transport_Management_Systems_Portal_REST_API.Data.Interfaces
+{
+    public interface IAuthenticationRepo
+    {
+        Task<bool> SaveChangesAsync();
+
+        Task CreateUserAsync(User user);
+
+        Task<User?> GetUserAsync(string email, string password);
+    }
+}
---
+++ Data/Repositories/AuthenticationRepo.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Transport_Management_Systems_Portal_REST_API.Data.Interfaces;
+using Transport_Management_Systems_Portal_REST_API.Models;
+
+namespace Transport_Management_Systems_Portal_REST_API.Data.Repositories
+{
+    public class AuthenticationRepo : IAuthenticationRepo
+    {
+        private readonly TMSDbContext _dbContext;
+
+        public AuthenticationRepo(TMSDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+        }
+
+        public async Task<User?> GetUserAsync(string email, string password)
+        {
+            var searchedResult = await _dbContext.Users.FirstOrDefaultAsync(user => user.Email == email && user.Password == password);
+
+            return searchedResult;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+            => await _dbContext.SaveChangesAsync() >= 0;
+    }
+}
