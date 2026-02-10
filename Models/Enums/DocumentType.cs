641e5e3ac9b8ae63806a8b550a9cc0488e105a8c
Adrian Joseph <adrianignatius15@hotmail.com>
2026-02-10 12:21:51 +0800

    Created enums to be used as categories/status/events for documents, tracking of shipment & orders

---
+++ Models/Enums/DocumentType.cs
@@ -0,0 +1,11 @@
+namespace Transport_Management_Systems_Portal_REST_API.Models.Enums
+{
+    public enum DocumentType
+    {
+        Waybill,
+        POD,
+        Invoice,
+        Label,
+        Other
+    }
+}
---
+++ Models/Enums/OrderStatus.cs
@@ -0,0 +1,13 @@
+namespace Transport_Management_Systems_Portal_REST_API.Models.Enums
+{
+    public enum OrderStatus
+    {
+        Created,
+        ReadyForPickup,
+        InTransit,
+        OutForDelivery,
+        Delivered,
+        Failed,
+        Cancelled
+    }
+}
